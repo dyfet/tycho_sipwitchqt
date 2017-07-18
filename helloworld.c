821315176b5d75f2a5a6a26dda6d86774113a710
Marko Shiva Pavlovic - in1t3r <marko.shiva.pavlovic@gmail.com>
2017-07-18 17:59:28 +0200

    changing content running pipeline

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  printf("Hello World!\n");
-  puts("joking around");
+  printf("Hello World we use Docker for CI/CD!\n");
   printf("Lets see whether the CI will work with merge.\n");
   return 0;
 }
