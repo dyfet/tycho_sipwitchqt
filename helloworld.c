3be7e488185c8c0f76842db09b25c9c73c17e360
Marko Shiva Pavlovic - in1t3r <marko.shiva.pavlovic@gmail.com>
2017-07-18 18:03:28 +0200

    changing content

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  printf("Hello World we use Docker for CI/CD!\n");
+  printf("Hello World we use Docker for CI/CD, trying kubernetees!\n");
   printf("Lets see whether the CI will work with merge.\n");
   return 0;
 }
