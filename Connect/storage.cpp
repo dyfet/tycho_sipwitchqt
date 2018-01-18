bf005b2c12a12dc6dc30f6821b28256c2aebdfbc
David Sugar <tychosoft@gmail.com>
2018-01-17 23:51:31 -0500

    server and port update, but not ext or label

--- Connect/storage.cpp
+++ Connect/storage.cpp
@@ -227,8 +227,8 @@ void Storage::updateCredentials(const QHash<QString,QString> &update)
     auto cred = credentials();
     foreach(auto key, update.keys())
         cred[key] = update[key];
-    runQuery("UPDATE Credentials SET user=?, secret=?, type=?, realm=? WHERE id=1;",
-        {cred["user"], cred["secret"], cred["type"], cred["realm"]});
+    runQuery("UPDATE Credentials SET server=?, port=?, user=?, secret=?, type=?, realm=? WHERE id=1;",
+        {cred["server"], cred["port"], cred["user"], cred["secret"], cred["type"], cred["realm"]});
 }
 
 bool Storage::exists()
