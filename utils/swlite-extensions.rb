029d6c3c545ac585c80b8cbec836a2aa3c4fa875
David Sugar <tychosoft@gmail.com>
2018-02-11 13:23:34 -0500

    more utility scripts

--- utils/swlite-extensions.rb
+++ utils/swlite-extensions.rb
@@ -10,15 +10,15 @@
 dbname = '/var/lib/sipwitchqt/local.db'
 dbname = '../testdata/local.db' if File.writable?('../testdata/local.db')
 dbname = '../userdata/local.db' if File.writable?('../userdata/local.db')
-abort("*** swlite-endpoints: no database") unless File.writable?(dbname)
+abort("*** swlite-extensions: no database") unless File.writable?(dbname)
 
 begin
   db = SQLite3::Database.open dbname
-  db.execute("SELECT number, label FROM Endpoints ORDER BY number, label") do |row|
-    puts "%-4s %-8s" % row
+  db.execute("SELECT number, name, display FROM Extensions ORDER BY number") do |row|
+    puts "%-4s %-32s %s" % row
   end
 rescue SQLite3::BusyException
-  abort("*** swlite-endpoints: database busy; sipwitchqt active")
+  abort("*** swlite-extensions: database busy; sipwitchqt active")
 rescue SQLite3::SQLException
-  abort("*** swlite-endpoints: database error")
+  abort("*** swlite-extensions: database error")
 end
