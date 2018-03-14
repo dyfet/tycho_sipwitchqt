40377f2f0a96c983b46f05570c997abc0b037f1e
Marko Shiva Pavlovic <marko.shiva.pavlovic@gmail.com>
2018-03-14 02:08:49 +0000

    Resolve "I want to know what devices are on my extension"

--- Dialogs/devicelist.cpp
+++ Dialogs/devicelist.cpp
@@ -13,7 +13,6 @@ QDialog(parent, Qt::Popup|Qt::WindowTitleHint|Qt::WindowCloseButtonHint)
 {
     ui.setupUi(static_cast<QDialog*>(this));
     if(connector != nullptr) {
-        //TODO: connect to receive server reply...
         connector->requestDeviceList();
         connect(connector, &Connector::changeDeviceList, this, [=](const QByteArray& json){
                     auto jdoc = QJsonDocument::fromJson(json);
@@ -23,8 +22,9 @@ QDialog(parent, Qt::Popup|Qt::WindowTitleHint|Qt::WindowCloseButtonHint)
             ui.table->setRowCount(1);
             int column = 0;
             int row = 0;
+            ui.table->setEditTriggers(QAbstractItemView::NoEditTriggers);
 
-
+            ui.table->setSizeAdjustPolicy(QTableWidget::AdjustToContents);
             foreach(auto profile, list) {
 
                         auto json = profile.toObject();
@@ -35,7 +35,6 @@ QDialog(parent, Qt::Popup|Qt::WindowTitleHint|Qt::WindowCloseButtonHint)
                         auto registered = json.value("r").toString();
                         auto agent = json.value("a").toString();
                         QStringList strArr = {endpoint, number,label,agent, registered, lastOnline};
-                        ui.table->setSizeAdjustPolicy(QTableWidget::AdjustToContents);
                         for (column = 0; column < totalCol; column++){
                             ui.table->setItem(row,column, new QTableWidgetItem(strArr[column]));
                         }
@@ -44,23 +43,17 @@ QDialog(parent, Qt::Popup|Qt::WindowTitleHint|Qt::WindowCloseButtonHint)
                     }
             ui.table->removeRow(ui.table->rowCount() - 1);
 //            auto count = ui.table->rowCount();
-            auto count = row;
-
-            ui.connectedDevices->setText(QString(count));
-                });
+            ui.connectedDevices->setText(QString::number(ui.table->rowCount()));
+            });
 }
 
     connect(ui.closeButton, &QPushButton::clicked, parent, &Desktop::closeDeviceList);
     show();
 }
 
-//QTableWidget tableWidget (int rows, int column, DeviceList *parent = 0)
-//{
-
-//}
-
-
 void DeviceList::closeEvent(QCloseEvent *event)
 {
-    Desktop::instance()->closeDialog();
+    event->ignore();
+
+//    Desktop::instance()->closeDialog();
 }
