fa7cb34db2ad2bf85c676cb24d6d76c646e05b0f
David Sugar <tychosoft@gmail.com>
2018-04-20 12:55:55 +0000

    Start of server debugging framework

--- testdata/siptest.sh
+++ testdata/siptest.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+wd=`dirname $0`
+cd $wd
 case "$1" in
 ping)
 	exec sipp 127.0.0.1:4060 -sf ping.xml -p 4050 -m 5 -s 30
 	;;
 default)
-	echo "siptest: unknown test" >&2
+	echo "siptest: $1: unknown test" >&2
 	exit 1
 	;;
 esac
