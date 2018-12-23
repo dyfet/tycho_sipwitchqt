2e0946be8c449cb9d8c47a757c26b4d9716d6ee4
David Sugar <tychosoft@gmail.com>
2018-12-23 17:25:07 -0500

    coverage and testing

--- testdata/siptest.sh
+++ testdata/siptest.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 wd=`dirname $0`
 cd $wd
-case "$1" in
+test=${1:-ping}
+case "$test" in
 ping)
 	exec sipp 127.0.0.1:4060 -sf ping.xml -p 4050 -m 5 -s 30
 	;;
-default)
+*)
 	echo "siptest: $1: unknown test" >&2
 	exit 1
 	;;
